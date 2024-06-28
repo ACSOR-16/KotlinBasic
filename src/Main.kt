00b0031b1fb788338f8f36ca9bb971b4e74bd30c
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2024-06-28 12:56:33 -0500

    firts commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+
+fun main() {
+    val name = "Kotlin"
+    var money : Int = 2;
+    money = 5;
+    println(money)
+
+    println("Hello, " + name + "!")
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
+}
