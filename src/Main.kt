bb2345700b7acda6313299fa6021fb598914c4cb
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2024-06-28 13:10:54 -0500

    variables

--- src/Main.kt
+++ src/Main.kt
@@ -10,4 +10,6 @@ fun main() {
     for (i in 1..5) {
         println("i = $i")
     }
+
+    variables();
 }
---
+++ src/Variables.kt
@@ -0,0 +1,17 @@
+fun variables() {
+    val boolean : Boolean= true;
+    val longNumber : Long = 2L;
+    val double : Double = 2.34;
+    val float : Float = 1.2f;
+
+    val firstValue = 14;
+
+    val secondValue = firstValue.minus(10);
+    val newValue = firstValue - secondValue;
+    println(secondValue);
+
+    val lastname : String = "Aguirre";
+    val name = "Oscar"
+    println("My name is $name $lastname")
+}
+
